1b446296399d0b254f854f2eeecf5af5ddec1000
StuartGrimes <17004808+StuartGrimes@users.noreply.github.com>
2017-11-12 18:39:20 +0000

    initial commit

--- src/com/groundanchor/HangManModel.java
+++ src/com/groundanchor/HangManModel.java
@@ -1,4 +1,48 @@
 package com.groundanchor;
 
+import javafx.scene.control.TextField;
+
 public class HangManModel {
+
+    private String[] wordList = {"litter", "beanbag", "opening", "settlement", "monkey", "turtle", "bigbang", "comic", "statement",
+            "iteration", "null", "integer", "iphone", "smartphone", "version", "control", "basic", "java", "head", "body"};
+    private String wordChoosen = wordList[(int) (Math.random() * wordList.length + 1)].toUpperCase();
+    private Integer noOfGuessesMade = 0;
+    private Integer livesLeft = 6;
+    private StringBuffer currentWordInPlayState = new StringBuffer();
+    private TextField guessLetter = new TextField();
+    private StringBuffer lettersGuessed= new StringBuffer();
+    private Boolean gameInPlay = true;
+
+    public HangManModel(){}
+
+
+
+    public String getWordChoosen() {
+        return wordChoosen;
+    }
+
+    public Integer getNoOfGuessesMade() {
+        return noOfGuessesMade;
+    }
+
+    public Integer getLivesLeft() {
+        return livesLeft;
+    }
+
+    public StringBuffer getCurrentWordInPlayState() {
+        return currentWordInPlayState;
+    }
+
+    public TextField getGuessLetter() {
+        return guessLetter;
+    }
+
+    public StringBuffer getLettersGuessed() {
+        return lettersGuessed;
+    }
+
+    public Boolean getGameInPlay() {
+        return gameInPlay;
+    }
 }
