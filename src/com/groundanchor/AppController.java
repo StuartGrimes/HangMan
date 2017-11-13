9589bf5d4930b0366b08f65d8c76f3f6e71aaadd
StuartGrimes <17004808+StuartGrimes@users.noreply.github.com>
2017-11-13 14:11:36 +0000

    GameSetupComplete

--- src/com/groundanchor/AppController.java
+++ src/com/groundanchor/AppController.java
@@ -1,25 +1,52 @@
 package com.groundanchor;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class AppController extends Application{
+public class AppController extends Application {
 
-    private HangManView view = new HangManView();
     private HangManModel model = new HangManModel();
+    private HangManView view = new HangManView();
+    Stage window = new Stage();
+
+    public AppController(){
+        view.setController(this);
+    }
+
+//    void setController() {
+//        view.setController(this);
+//    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try{
-            Stage window = primaryStage;
+        try {
+            window = primaryStage;
             view.setButtonText("Start Game");
-            view.getInitialView(window);
-        }catch(Exception e){
+            view.getView(window);
+        } catch (Exception e) {
             System.out.println("Heuston...we have a problem.");
         }
     }
 
+
+    void upDateGame() {
+        Integer lives = model.getLivesLeft();
+        Label wordDisplay = model.getWordDisplay();
+        String lettersGuessed = model.getLettersGuessed();
+        view.createCenter(lives, wordDisplay, lettersGuessed);
+        view.setButtonText("Process Guess");
+        view.getView(window);
+        System.out.println("Thank Fuck!");
+    }
+
     //deal with a button click - process guess and update state
+    void processGuess(ActionEvent event) {
+    Object happened = event;
+    }
+
+
 
 
 }
